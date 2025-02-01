3a8d077a61f6d17cf6b2b5e270737e2fdaa46b89
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-01 15:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let btn=document.getElementById("enterBtn");
+btn.addEventListener("click", function(){
+	let ans=document.getElementById("status");
+	ans.innerHTML="<h1>Entered Metaverse</h1>";
+})
